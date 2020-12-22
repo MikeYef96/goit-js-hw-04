ee67c425c2ef92c3a16cf7065fa9ef6724de68f1
MikeYef96 <mike.yef@gmail.com>
2020-12-22 12:29:48 +0300

    homework-4finish

---
+++ js/task-2.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// Исправь ошибки которые будут в консоли, чтобы скрипт заработал.
+
+const inventory = {
+  items: ["Knife", "Gas mask"],
+  add(itemName) {
+    console.log(this.items);
+    console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+    this.items.filter((items) => items !== itemName);
+  },
+};
+
+const invokeInventoryAction = function (itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction("Medkit", inventory.add.bind(inventory));
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction("Gas mask", inventory.remove.bind(inventory));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
